b88e580a6b2f5d4fe27d3263fd4937c7be6102de
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-25 15:37:44 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,8 @@
-def my_each # put argument(s) here
-  # code here
+# Building my own version of the .each method
+def my_each(array)
+  if block_given?
+
+  else
+    puts "No block given."
+  end
 end
